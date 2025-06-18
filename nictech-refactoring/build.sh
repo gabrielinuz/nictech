95b30ba2ab7bb8c0aa745fffd497039f8f6e5680
gabrielinuz <gabrielinuz@gmail.com>
2025-06-18 18:53:01 -0300

    cambios en build.sh: compilar todos los .cpp del directorio src

--- nictech-refactoring/build.sh
+++ nictech-refactoring/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir build
-g++ -Wall -std=c++17 -Iinclude src/main.cpp src/customer_manager.cpp src/console_utils.cpp -o build/nictech.exe
+# g++ -Wall -std=c++17 -Iinclude src/main.cpp src/customer_manager.cpp src/console_utils.cpp -o build/nictech.exe
+g++ -Wall -std=c++17 -Iinclude src/*.cpp -o build/nictech.exe
 chmod +x build/nictech.exe
